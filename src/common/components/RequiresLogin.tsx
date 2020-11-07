faa88509ccc52d411851199fa13f0a362dade2c0
Johannes Kvamme <22146437+FluidSense@users.noreply.github.com>
2020-11-07 22:56:58 +0100

    Center loading spinner

--- src/common/components/RequiresLogin.tsx
+++ src/common/components/RequiresLogin.tsx
@@ -1,10 +1,24 @@
 import { Spinner } from '@dotkomonline/design-system';
 import { useSession } from 'next-auth/client';
 import React from 'react';
+import styled from 'styled-components';
+
+const SpinnerWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 const RequiresLogin: React.FC = ({ children }) => {
   const [session, loading] = useSession();
-  if (loading) return <Spinner />;
+  if (loading)
+    return (
+      <SpinnerWrapper>
+        <Spinner />
+      </SpinnerWrapper>
+    );
   if (!session) return <p>Du må logge inn for å se denne siden.</p>;
   return <>{children}</>;
 };
